a7e1d96d3f8d800c5077407d245478f28945ca67
ikhanasif <ikhanasif1970@gmail.com>
2017-11-06 17:04:11 -0500

    modified default.rb

--- cookbooks/asif1/recipes/default.rb
+++ cookbooks/asif1/recipes/default.rb
@@ -3,8 +3,4 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-file '/var/index.php' do
-  content '<html>This is s my first cookbook install after bootstraping agent1
- home page.</html>'
-  mode '0755'
-end
+package "git"
